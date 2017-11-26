a23ead1e058b6721254e5df1932b61082d50dbc9
ankatus <antti.j.kataja@gmail.com>
2017-11-26 14:18:38 +0200

    lisätty tarkastelunäkymä ja parenneltu toiminnallisuutta muutenkin

--- src/main/java/user_interface/TextUI.java
+++ src/main/java/user_interface/TextUI.java
@@ -4,6 +4,8 @@ import data_access.DbKirjaDAO;
 import domain.Kirja;
 import domain.Lukuvinkki;
 import java.sql.SQLException;
+import java.util.HashMap;
+
 import data_access.KirjaDAO;
 
 public class TextUI {
@@ -16,12 +18,12 @@ public class TextUI {
         this.dao = dao;
     }
 
-    public void addRun() throws SQLException {
+    private void addRun() throws SQLException {
         addloop:
         while (true) {
 
             io.println("Valitse lisättävä tyyppi");
-            io.println("Komento (1=kirja, x=palaa)");
+            io.println("Komento (1=kirja, x=palaa):");
             String input = io.nextLine();
 
             switch (input) {
@@ -58,13 +60,8 @@ public class TextUI {
                     break;
 
                 case "2":
-                    int index = 1;
-                    for (Lukuvinkki l : dao.getAll()) {
-                        io.println(index + ". " + l);
-                        io.println();
-                        index++;
-                    }
-                    io.println();
+                    HashMap indexMap = list();
+                    chooseLukuvinkki(indexMap);
                     break;
 
                 case "x":
@@ -79,6 +76,40 @@ public class TextUI {
         io.println("Kiitos ja näkemiin!");
     }
 
+    //ottaa parametrinaan HashMapin, joka sisältää listan indeksit ja niitä vastaavat lukuvinkit
+    private void chooseLukuvinkki(HashMap<Integer, Lukuvinkki> indexMap) {
+        while (true) {
+            System.out.println();
+            System.out.println("Haluatko tarkastella lukuvinkkiä?");
+            System.out.println("Anna kohteen indeksi, listaa uudestaan (\"l\") tai palaa (\"x\")");
+            String input = io.nextLine();
+
+            if (input.toLowerCase().equals("x")) {
+                return;
+            }
+            if (input.toLowerCase().equals("l")) {
+                list();
+                continue;
+            }
+            int wantedIndex;
+            try {
+                wantedIndex = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Epäkelpo komento");
+                continue;
+            }
+
+            if (indexMap.containsKey(wantedIndex)) {
+                viewLukuvinkki(indexMap.get(wantedIndex));
+            } else {
+                System.out.println("Ei lukuvinkkiä tällä indeksillä");
+                continue;
+            }
+        }
+
+        
+    }
+
     private void addBook() throws SQLException {
         io.println("Kirjan nimi: ");
         String otsikko = io.nextLine();
@@ -88,6 +119,32 @@ public class TextUI {
         String isbn = io.nextLine();
         Kirja kirja = new Kirja(otsikko, kirjoittaja, isbn);
         dao.save(kirja);
+    }
 
+    //listaa lukuvinkit ja palauttaa HashMapin, jossa avaimina listan indeksit ja arvoina lukuvinkit
+    private HashMap<Integer, Lukuvinkki> list() {
+        HashMap<Integer, Lukuvinkki> indexMap = new HashMap<>();
+        int index = 1;
+        for (Lukuvinkki l : dao.getAll()) {
+            io.println(index + ". " + l);
+            indexMap.put(index, l);
+            index++;
+        }
+        io.println();
+        return indexMap;
+    }
+
+    private void viewLukuvinkki(Lukuvinkki l) {
+        System.out.println(l);
+        while (true) {
+            System.out.println();
+            System.out.println("Komento (x=palaa):");
+            String input = io.nextLine();
+            if (input.toLowerCase().equals("x")) {
+                return;
+            } else {
+                System.out.println("Epäkelpo komento");
+            }
+        }
     }
 }
