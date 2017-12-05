49875870b05ffa4bd8db2d2e192c24bc4fc1fc81
ankatus <antti.j.kataja@gmail.com>
2017-12-05 15:20:53 +0200

    muuteltu juttuja käyttöliittymän refaktoroinnin yhteydessä

--- src/main/java/tools/TextTools.java
+++ src/main/java/tools/TextTools.java
@@ -1,5 +1,7 @@
 package tools;
 
+import domain.Type;
+
 public class TextTools {
 
     public static String fit(String s, int length) {
@@ -22,12 +24,35 @@ public class TextTools {
         return s;
     }
 
-    public static String createHeaders(int length, String... headers) {
-        String headersRow = "";
+
+    public static String createLabelForType(Type type) {
+        if (type == Type.KIRJA) {
+            return "Kirjat:";
+        } else if (type == Type.BLOGI) {
+            return "Blogit:";
+        } else {
+            return null;//uusia tyyppejä odotellessa
+        }
+    }
+
+    private static String createHeaders(int length, String... headers) {
+        String headersRow = fit("id", 5);
         for (String s : headers) {
             headersRow += fit(s, length);
             headersRow += "   ";
         }
         return headersRow;
+    }
+
+    public static String createHeadersForType(int length, Type type) {
+        String[] headers;
+        if (type == Type.KIRJA) {
+            headers = new String[]{"kirjoittaja", "otsikko", "ISBN", "luettu"};
+        } else if (type == Type.BLOGI) {
+            headers = new String[]{"kirjoittaja", "otsikko", "url", "luettu"};
+        } else {
+            headers = new String[]{};//uusia tyyppejä odotellessa
+        }
+        return createHeaders(length, headers);
     }
 }
