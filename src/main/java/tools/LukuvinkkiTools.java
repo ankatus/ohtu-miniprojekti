d24da74c3406a3d3534da571048f557d77192553
ankatus <antti.j.kataja@gmail.com>
2017-12-05 15:18:31 +0200

    apumetodeita lukuvinkkien listaukseen ja järjestelyyn

---
+++ src/main/java/tools/LukuvinkkiTools.java
@@ -0,0 +1,78 @@
+package tools;
+
+import domain.IndexIdPair;
+import domain.Lukuvinkki;
+import domain.Type;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class LukuvinkkiTools {
+
+
+    public static HashMap<Type, ArrayList<IndexIdPair>> pairListsByType(ArrayList<Lukuvinkki> lukuvinkkiList) {
+        HashMap<Type, ArrayList<IndexIdPair>> mapOfLists = new HashMap<>();
+        int index = 1;
+        for (Type type : Type.values()) {
+            mapOfLists.put(type, pairListSingleType(lukuvinkkiList, type, index));
+            index += mapOfLists.get(type).size() + 1;//inkrementoidaan indeksiä niin tarpeeksi
+        }
+        return mapOfLists;
+    }
+
+    public static Lukuvinkki getLukuvinkkiById(String id, ArrayList<Lukuvinkki> lukuvinkkiList) {
+        for (Lukuvinkki l : lukuvinkkiList) {
+            if (l.getID().equals(id)) {
+                return l;
+            }
+        }
+        return null;
+    }
+//
+//    public static Lukuvinkki getLukuvinkkiByIndex(int index, HashMap<Type, ArrayList<IndexIdPair>> map, ArrayList<Lukuvinkki> lukuvinkkiList) {
+//        for (ArrayList<IndexIdPair> list : map.values()) {
+//            for (IndexIdPair pair : list) {
+//                if (pair.getIndex() == index) {
+//                    return getLukuvinkkiById(pair.getId(), lukuvinkkiList);
+//                }
+//            }
+//        }
+//        return null;
+//    }
+
+    public static String getLukuvinkkiIdByIndex(int index, HashMap<Type, ArrayList<IndexIdPair>> map) {
+        for (ArrayList<IndexIdPair> list : map.values()) {
+            for (IndexIdPair pair : list) {
+                if (pair.getIndex() == index) {
+                    return pair.getId();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static Type parseTypeFromId(String id) {
+        String typeString = id.substring(0,1);
+        switch (typeString) {
+            case "K":
+                return Type.KIRJA;
+            case "B":
+                return Type.BLOGI;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private static ArrayList<IndexIdPair> pairListSingleType(ArrayList<Lukuvinkki> lukuvinkkiList, Type type, int startIndex) {
+        ArrayList<IndexIdPair> pairList = new ArrayList<>();
+        for (Lukuvinkki l : lukuvinkkiList) {
+            if (l.getType() == type) {
+                pairList.add(new IndexIdPair(startIndex, l.getID()));
+            }
+        }
+        return pairList;
+    }
+
+
+
+}
