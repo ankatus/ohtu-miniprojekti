eeb7e0eb10c68635938f613de3d6a09062c95eac
Valtteri <valtsu54@hotmail.com>
2017-12-11 15:45:05 +0200

    videodao

--- src/main/java/data_access/VideoDAO.java
+++ src/main/java/data_access/VideoDAO.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package data_access;
 
-/**
- *
- * @author vavo
- */
+import domain.Video;
+import domain.Lukuvinkki;
+import java.util.ArrayList;
+
 public interface VideoDAO {
-    
+    ArrayList<Lukuvinkki> getAll();
+    void save(Video video);
+    void markAsLuettu(String id);
 }
