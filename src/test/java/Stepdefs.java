2acbc2cd032476f27623a9f9ab34468cc311eb9d
jurintal1 <jussi.rintala@gmail.com>
2017-12-13 13:28:00 +0200

    testejä korjattu

--- src/test/java/Stepdefs.java
+++ src/test/java/Stepdefs.java
@@ -1,16 +1,10 @@
 import org.junit.Before;
 import user_interface.*;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import data_access.Database;
-import data_access.DbBlogiDAO;
-import data_access.DbKirjaDAO;
-import data_access.DbKommenttiDAO;
-import data_access.DbPodcastDAO;
-import data_access.DbVideoDAO;
-import data_access.MasterDAO;
+import data_access.*;
+
 /*
 import java.io.File;
 import java.io.FileWriter;
@@ -37,6 +31,18 @@ public class Stepdefs {
     DbVideoDAO videoDAO;
     DbPodcastDAO podcastDAO;
     String randomTitle;
+    
+        @cucumber.api.java.Before
+    public void setUp() throws ClassNotFoundException {
+        inputLines = new ArrayList<>();
+        database = new Database("testitietokanta.db");
+        kirjaDAO = new DbKirjaDAO(database);
+        kommenttiDAO = new DbKommenttiDAO(database);
+        blogiDAO = new DbBlogiDAO(database);
+        videoDAO = new DbVideoDAO(database);
+        podcastDAO = new DbPodcastDAO(database);
+        dao = new MasterDAO(kirjaDAO, blogiDAO, kommenttiDAO, videoDAO, podcastDAO);       
+    }
 
     @Given("^system is asking input from user$")
     public void system_is_asking_input_from_user() throws Throwable {
@@ -48,14 +54,14 @@ public class Stepdefs {
         videoDAO = new DbVideoDAO(database);
         podcastDAO = new DbPodcastDAO(database);
         dao = new MasterDAO(kirjaDAO, blogiDAO, kommenttiDAO, videoDAO, podcastDAO);
-        inputLines[0] = "2"; // Tällä siirrytään listaukseen
+        inputLines.add("2"); // Tällä siirrytään listaukseen
     }
 
     @When("^empty string is entered$")
     public void empty_string_is_entered() throws Throwable {
-        inputLines[1] = ""; // Sitten palataan
-        inputLines[2] = ""; // ja poistutaan ohjelmasta
-        io = new StubIO(inputLines);
+        inputLines.add("1"); // Sitten palataan
+        inputLines.add(""); // ja poistutaan ohjelmasta
+        io = new StubIO(inputLines.toArray(new String[]{}));
         ui = new TextUI(io, dao);
         ui.run();
     }
@@ -72,28 +78,17 @@ public class Stepdefs {
     @Given("^listed book details and chosed book to mark as read$")
     public void input_for_adding_book_and_marking_it_as_read() throws Throwable {
         database = new Database("lukuvinkkikirjasto.db");
-
-
-    
-
-
-    @cucumber.api.java.Before
-    public void setUp() throws ClassNotFoundException {
-        inputLines = new ArrayList<>();
-        database = new Database("testdb.db");
-        kirjaDAO = new DbKirjaDAO(database);
-        kommenttiDAO = new DbKommenttiDAO(database);
-        blogiDAO = new DbBlogiDAO(database);
-        videoDAO = new DbVideoDAO(database);
-        dao = new MasterDAO(kirjaDAO, blogiDAO, kommenttiDAO, videoDAO, podcastDAO);       
     }
+
+
+
 
     @When("^marked as read$")
     public void marked_as_read() throws Throwable {
-        inputLines[2] = "m";
-        inputLines[3] = "";
-        inputLines[4] = "";
-        io = new StubIO(inputLines);
+        inputLines.add("m");
+        inputLines.add("");
+        inputLines.add("");
+        io = new StubIO(inputLines.toArray(new String[]{}));
         ui = new TextUI(io, dao);
         ui.run();
     }
@@ -193,7 +188,7 @@ public class Stepdefs {
 
 
     @Given("^list view is entered$")
-    public void list_view_is_entered() {
+    public void list_view_is_selected() {
         inputLines.add("2");
 
     }
@@ -219,7 +214,7 @@ public class Stepdefs {
         inputLines.add("");
         inputLines.add("");//jotta ohjelma sulkeutuu...
         io = new StubIO(inputLines.toArray(new String[]{}));
-        TextUI ui = new TextUI(io, dao);
+        ui = new TextUI(io, dao);
         ui.run();
         for (int i = 0; i < io.getOutputs().size(); i++) {
             if (io.getOutputs().get(i).contains(arg1) && io.getOutputs().get(i).contains(arg2)) {
@@ -233,14 +228,14 @@ public class Stepdefs {
 
     @When("^create new lukuvinkki is selected$")
     public void create_new_lukuvinkki_is_selected() throws Throwable {
-        inputLines[0] = "1";
+        inputLines.add("1");
     }
 
     @Then("^podcast is available in the menu$")
     public void podcast_is_available_in_the_menu() throws Throwable {
-        inputLines[1] = "";
-        inputLines[2] = "";
-        io = new StubIO(inputLines);
+        inputLines.add("");
+        inputLines.add("");
+        io = new StubIO(inputLines.toArray(new String[]{}));
         ui = new TextUI(io, dao);
         ui.run();
         assertTrue(io.getOutputs().contains("Komento (1=kirja, 2=blogi, 3=video, 4=podcast, \"\"=palaa):"));
@@ -248,27 +243,27 @@ public class Stepdefs {
 
     @Given("^creating a new podcast is selected$")
     public void creating_a_new_podcast_is_selected() throws Throwable {
-        inputLines[1] = "4";
+        inputLines.add("4");
     }
 
     @Given("^author \"([^\"]*)\" a random title and URL \"([^\"]*)\" are entered$")
     public void author_a_random_title_and_URL_are_entered(String author, String url) throws Throwable {
         randomTitle = UUID.randomUUID().toString();
-        inputLines[2] = randomTitle;
-        inputLines[3] = author;
-        inputLines[4] = url;
+        inputLines.add(randomTitle);
+        inputLines.add(author);
+        inputLines.add(url);
     }
 
     @When("^all items are listed$")
     public void all_items_are_listed() throws Throwable {
-        inputLines[5] = "2";
+        inputLines.add("2");
     }
 
     @Then("^the random title appears on the list$")
     public void the_random_title_appears_on_the_list() throws Throwable {
-        inputLines[6] = "";
-        inputLines[7] = "";
-        io = new StubIO(inputLines);
+        inputLines.add("");
+        inputLines.add("");
+        io = new StubIO(inputLines.toArray(new String[] {}));
         ui = new TextUI(io, dao);
         ui.run();
         assertTrue(outputsContains(io, randomTitle.substring(0, 19)));
--- src/test/resources/Podcast.feature
+++ src/test/resources/Podcast.feature
@@ -1,14 +1,12 @@
 Feature: User can save a podcast
 
 Scenario: creating a new podcast is available in the menu
-Given system is asking input from user
 When create new lukuvinkki is selected
 Then podcast is available in the menu
 
 Scenario: podcast information can be saved
-Given system is asking input from user
-And create new lukuvinkki is selected
+Given create new lukuvinkki is selected
 And creating a new podcast is selected
-And author "Longworth, Karina" a random title and URL "http://www.youmustrememberthispodcast.com" are entered
+And author "Longworthh, Karina" a random title and URL "http://www.youmustrememberthispodcast.com" are entered
 When all items are listed
 Then the random title appears on the list
