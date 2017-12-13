d7634573e121a0ea93a7431ba3276426314167f6
Valtteri <valtsu54@hotmail.com>
2017-12-13 20:49:25 +0200

    tietokanta luodaan jos sitä ei ole

--- src/main/java/data_access/Database.java
+++ src/main/java/data_access/Database.java
@@ -1,7 +1,11 @@
 package data_access;
 
+import java.io.File;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Database {
 
@@ -13,6 +17,77 @@ public class Database {
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;
 
+        if (!checkIfDatabaseExists(databaseAddress)) {
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            createDatabase(databaseAddress);
+        }
+    }
+
+    public boolean checkIfDatabaseExists(String databaseName) {
+        File f = new File(databaseName);
+        return f.exists();
+    }
+
+    public void createDatabase(String databaseName) {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + databaseAddress);
+            
+            executeQueryUpdate("CREATE TABLE Kommentti(\n"
+                    + "lukuvinkki TEXT, \n"
+                    + "kommentoija TEXT,\n"
+                    + "kommentti TEXT,\n"
+                    + "aika DATETIME DEFAULT (strftime('%H:%M %d.%m.%Y','now', 'localtime'))\n"
+                    + ");", new ArrayList());
+                    
+            executeQueryUpdate("CREATE TABLE Kirja (\n"
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                    + "otsikko VARCHAR(100) NOT NULL,\n"
+                    + "kirjoittaja VARCHAR(100),\n"
+                    + "isbn VARCHAR(100),\n"
+                    + "luettu BOOLEAN NOT NULL DEFAULT 0\n"
+                    + ");", new ArrayList());
+            
+            executeQueryUpdate("CREATE TABLE Blogi (\n"
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                    + "otsikko VARCHAR(100) NOT NULL,\n"
+                    + "kirjoittaja VARCHAR(100),\n"
+                    + "url VARCHAR(100),\n"
+                    + "luettu BOOLEAN NOT NULL DEFAULT 0\n"
+                    + ");", new ArrayList());
+            
+            executeQueryUpdate("CREATE TABLE Video (\n"
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                    + "otsikko VARCHAR(100) NOT NULL,\n"
+                    + "tekija VARCHAR(100),\n"
+                    + "url VARCHAR(100),\n"
+                    + "katsottu BOOLEAN NOT NULL DEFAULT 0\n"
+                    + ");", new ArrayList());
+            
+            executeQueryUpdate("CREATE TABLE Podcast (\n"
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                    + "otsikko VARCHAR(100) NOT NULL,\n"
+                    + "tekija VARCHAR(100),\n"
+                    + "url VARCHAR(100),\n"
+                    + "kuunneltu BOOLEAN NOT NULL DEFAULT 0\n"
+                    + ");", new ArrayList());
+            
+            executeQueryUpdate("CREATE TABLE Tag (\n"
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                    + "tag VARCHAR(100) NOT NULL\n"
+                    + ");", new ArrayList());
+            
+            executeQueryUpdate("CREATE TABLE Taglink (\n"
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                    + "lukuvinkki TEXT,\n"
+                    + "tagid INTEGER,\n"
+                    + "FOREIGN KEY(tagid) REFERENCES Tag(id)\n"
+                    + ");", new ArrayList());
+            
+            closeConnection();
+            
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
     }
 
     public Connection connect() {
@@ -49,12 +124,12 @@ public class Database {
             connection = this.connect();
 
             stmt = connection.prepareStatement(query);
-            
+
             int i = values.size();
-            for (int j = 0; j<i; j++) {
-                stmt.setObject(j+1, values.get(j));
+            for (int j = 0; j < i; j++) {
+                stmt.setObject(j + 1, values.get(j));
             }
-            
+
             rS = stmt.executeQuery();
 
             return rS;
@@ -70,12 +145,12 @@ public class Database {
             connection = this.connect();
 
             stmt = connection.prepareStatement(query);
-            
+
             int i = values.size();
-            for (int j = 0; j<i; j++) {
-                stmt.setObject(j+1, values.get(j));
+            for (int j = 0; j < i; j++) {
+                stmt.setObject(j + 1, values.get(j));
             }
-            
+
             stmt.executeUpdate();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
